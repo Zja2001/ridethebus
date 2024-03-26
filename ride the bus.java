fb15a26000eb9d53ea76763fcd1694fbf3913cd0
Zja2001 <112417653+Zja2001@users.noreply.github.com>
2024-03-26 19:59:18 -0400

    Add files via upload

---
+++ ride the bus.java
@@ -0,0 +1,17 @@
+import java.io;
+import java.util;
+
+public class ridethebuss 
+{
+    public static void main(String[] args)
+    {
+        arraylist<String> cards = new arraylist<String>();
+        
+        For(int i = 0; i < 52; i++)
+        {
+            Object card = new Object(i, "hearts");
+            arraylist.add(card);
+        }
+        System.out.println(cards);
+    }
+}
